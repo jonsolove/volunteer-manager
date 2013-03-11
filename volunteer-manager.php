3f16ea3cfa668ca977fabe5edccab9a4bbc51451
Jon Solove <jon.solove@gmail.com>
2013-03-10 21:09:53 -0600

    initial commit.

--- volunteer-manager.php
+++ volunteer-manager.php
@@ -14,16 +14,17 @@ add_action( 'admin_menu', 'my_plugin_menu' );
 
 /** Step 1. */
 function my_plugin_menu() {
-	add_dashboard_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+	add_options_page( 'Volunteer Manager Options', 'Volunteer Manager', 'manage_options', 'volman', 'my_plugin_options' );
 }
 
 /** Step 3. */
 function my_plugin_options() {
+	
 	if ( !current_user_can( 'manage_options' ) )  {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
 	echo '<div class="wrap">';
-	echo '<p>Here is where the form would go if I actually had options.</p>';
+	echo '<p>Volunteer Manager Options will live here.</p>';
 	echo '</div>';
 }
 
