f9419831a871e39dc249876b6b9db62c75edde6d
Jon Solove <jon.solove@gmail.com>
2013-03-10 23:59:04 -0600

    getting rid of unecessary line...

--- volunteer-profile.php
+++ volunteer-profile.php
@@ -6,5 +6,4 @@ $current_user = wp_get_current_user();
 <h1>Profile</h1>
 <p><?php 
 	$current_user = wp_get_current_user();
-	$random_string = 'hello';
 	echo $current_user->first_name . " " . $current_user->last_name; ?></p>
